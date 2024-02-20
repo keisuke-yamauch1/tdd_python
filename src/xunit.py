76d2554503031a3ee9987e00c388ce9d9cc40939
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-20 21:46:27 +0900

    update: テスト名をもとにメソッド実行する

--- src/xunit.py
+++ src/xunit.py
@@ -1,6 +1,11 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
 
     def testMethod(self):
         self.wasRun = 1
