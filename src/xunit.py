1090c32f04df08a4d03906d7274cd6a811f26fcb
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-24 14:45:49 +0900

    add: TestSuite

--- src/xunit.py
+++ src/xunit.py
@@ -50,6 +50,14 @@ class TestResult:
         return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
@@ -72,8 +80,16 @@ class TestCaseTest(TestCase):
         result.testFailed()
         assert "1 run, 1 failed" == result.summary()
 
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        result = suite.run()
+        assert "2 run, 1 failed" == result.summary()
+
 
 print(TestCaseTest("testTemplateMethod").run().summary())
 print(TestCaseTest("testResult").run().summary())
 print(TestCaseTest("testFailedResult").run().summary())
 print(TestCaseTest("testFailedResultFormatting").run().summary())
+print(TestCaseTest("testSuite").run().summary())
