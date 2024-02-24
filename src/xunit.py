dbf4620c779f26c5ad6acda3d4059194d6990cc0
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-24 14:47:18 +0900

    update: runメソッド作成

--- src/xunit.py
+++ src/xunit.py
@@ -57,6 +57,11 @@ class TestSuite:
     def add(self, test):
         self.tests.append(test)
 
+    def run(self):
+        result = TestResult()
+        for test in self.tests:
+            test.run(result)
+
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
