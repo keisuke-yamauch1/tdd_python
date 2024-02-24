9b56543ce9e19ead46747776346ff794cc37564a
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-24 14:51:07 +0900

    update: テスト実行をsuiteを使用する

--- src/xunit.py
+++ src/xunit.py
@@ -8,8 +8,7 @@ class TestCase:
     def tearDown(self):
         pass
 
-    def run(self):
-        result = TestResult()
+    def run(self, result):
         result.testStarted()
         self.setUp()
         try:
@@ -18,7 +17,6 @@ class TestCase:
         except:
             result.testFailed()
         self.tearDown()
-        return result
 
 
 class WasRun(TestCase):
@@ -57,8 +55,7 @@ class TestSuite:
     def add(self, test):
         self.tests.append(test)
 
-    def run(self):
-        result = TestResult()
+    def run(self, result):
         for test in self.tests:
             test.run(result)
 
@@ -94,8 +91,12 @@ class TestCaseTest(TestCase):
         assert "2 run, 1 failed" == result.summary()
 
 
-print(TestCaseTest("testTemplateMethod").run().summary())
-print(TestCaseTest("testResult").run().summary())
-print(TestCaseTest("testFailedResult").run().summary())
-print(TestCaseTest("testFailedResultFormatting").run().summary())
-print(TestCaseTest("testSuite").run().summary())
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testSuite"))
+result = TestResult()
+suite.run(result)
+print(result.summary())
